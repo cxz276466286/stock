fbca9fd009e69b8fb3f127ed7cb0eb7e2ddea7ae
cz <276466286@qq.com>
2022-07-17 16:47:16 +0800

    support multiple stock

--- scripts/warn_limit_price.py
+++ scripts/warn_limit_price.py
@@ -1,8 +1,6 @@
-from calendar import month
 import tushare as ts
 import time
 import winsound
-from datetime import date, timedelta
 
 codes = ['000957']
 warning_v = 10000
@@ -10,15 +8,16 @@ warning_v = 10000
 def watch():
     while True:
         data = ts.get_realtime_quotes(codes)
-        # print(data)
-        b1_v = float(data['b1_v'][0]) if bool(data['b1_v'][0]) else 0
-        a1_v = float(data['a1_v'][0]) if bool(data['a1_v'][0]) else 0
-        name = data['name'][0]
-        print(name, b1_v, a1_v, warning_v)
+        for i, code in enumerate(codes):
+            # print(data)
+            b1_v = int(data['b1_v'][i]) if bool(data['b1_v'][i]) else 0
+            a1_v = int(data['a1_v'][i]) if bool(data['a1_v'][i]) else 0
+            name = data['name'][i]
+            print(name, b1_v, a1_v, warning_v)
 
-        if b1_v < warning_v and a1_v < warning_v:
-            print('WARNING:', name)
-            winsound.Beep(500, 800)
+            if b1_v < warning_v and a1_v < warning_v:
+                print('WARNING:', name, code)
+                winsound.Beep(500, 800)
 
         time.sleep(2.5)
 
