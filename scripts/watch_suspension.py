8102b1562f7a8254002f5640792a88d4dae0b071
cz <276466286@qq.com>
2022-07-18 12:53:49 +0800

    warning order

--- scripts/watch_suspension.py
+++ scripts/watch_suspension.py
@@ -3,7 +3,7 @@ import time
 import winsound
 from datetime import date, timedelta
 
-codes = ['000957', '002090']
+codes = ['000957']
 index_codes = ['399106']
 # stock_index_base = 2155.56
 # stock_base = 20.38
@@ -32,13 +32,13 @@ def watch():
                         (szzz - szzz_base) / szzz_base, 5)
             target_price = round((0.2 + (szzz - szzz_base) /
                                 szzz_base) * stock_base_list[i] + stock_base_list[i], 2)
-            if abs(rate) >= 0.19:
-                print('{} {}  {}  {}  \033[1;43m{}\033[0m'.format(
-                    name, stock_price, target_price, szzz, rate))
-            elif abs(rate) >= 0.2:
+            if abs(rate) >= 0.2:
                 print('{} {}  {}  {}  \033[1;41m{}\033[0m'.format(
                     name, stock_price, target_price, szzz, rate))
                 winsound.Beep(500, 800)
+            elif abs(rate) >= 0.19:
+                print('{} {}  {}  {}  \033[1;43m{}\033[0m'.format(
+                    name, stock_price, target_price, szzz, rate))
             else:
                 print('{} {}  {}  {}  \033[0;42m{}\033[0m'.format(
                     name, stock_price, target_price, szzz, rate))
